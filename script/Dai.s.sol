199561ac1fe10af342af9c0afdb04f22fd658310
superkidhedpay <cryptomusha@gmail.com>
2024-12-20 09:01:04 +0900

    deploy

--- script/Dai.s.sol
+++ script/Dai.s.sol
@@ -59,4 +59,17 @@ contract DeployScript is Script {
 
 // forge script script/Dai.s.sol:DeployScript --rpc-url $RPC_URL --broadcast  --etherscan-api-key $API_KEY --verify -vvvv
 
-// 0xe3e0448577652dA36556B4B467650e908D290612
+// [src/Dai.sol:Dai] 0x4a83eF3C50dae67691A79cfaEB00c45b8D851d5A
+// [src/Vat.sol:Vat] 0xEDD21ea2F824d11e89BdF936F3A4699A0FF2B705
+// [src/BatToken.sol:BAToken] 0x39Af0751eC28C70F61a320f732e2ecd681d406d0
+// [src/Join.sol:DaiJoin] 0xE26c2060B1001ab8346f1e8671348e9002e2caB7
+// [src/Join.sol:ETHJoin] 0x5BcFc3Ee30ECfC7014cB0f30d76CC2DFE8b8263F
+// [src/Join.sol:GemJoin] 0xD6eE66e6a02bE468e38DEa63Cf0110Bba2A5956d
+// [src/Spotter.sol:Spotter] 0x29C6Bb776193f88aD2E5a2fe0efB4EA02C7A7863
+// [src/Dog.sol:Dog] 0x6B067D3ac0aBB51fd2B0C62D5fd1A28FFA5e7364
+// [src/Clipper.sol:Clipper] 0xFa45c9f9BF7d852d00dcFC525e7E3CaBdE5e1B66
+// [src/Abacus.sol:StairstepExponentialDecrease] 0x24fbe0C15C0928DC904Fed11e87D4dC59bb2F3A0
+// [src/Flapper.sol:Flapper] 0x9e40e1f724E2c7cDe2d0fa8a00b60cC474C52Cd9
+// [src/Flopper.sol:Flopper] 0x24d0fFe2EdbB223975531d78fbe94654B1b9BE73
+// [src/Vow.sol:Vow] 0xf8CA62C21bF239d711E09849104436ff528F00d2
+// [src/Token.sol:DSToken] 0x69AF45B7ed366b4656c8EaC7FBd2b0C52341Ef1c
